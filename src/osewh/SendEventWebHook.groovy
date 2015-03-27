77a02074dfd01a8334c79e45642be845a90fc640
Umberto Nicoletti <umberto.nicoletti@gmail.com>
2015-03-27 21:26:40 +0000

    make jetty port configurable

--- src/osewh/SendEventWebHook.groovy
+++ src/osewh/SendEventWebHook.groovy
@@ -4,14 +4,15 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.*;
 
 def startJetty() {
-    def jetty = new Server(9090)
+    def port=System.getProperty("PORT")?:"9090"
+    def jetty = new Server(port as int)
 
     def context = new ServletContextHandler(jetty, '/', ServletContextHandler.NO_SESSIONS)
     context.resourceBase = './web/'
     context.addServlet(new ServletHolder(new OSEWebHookServlet()), "/*");
 
+    println "Starting Jetty on 0.0.0.0:${port}, press Ctrl+C to stop."
     jetty.start()
 }
 
-println "Starting Jetty, press Ctrl+C to stop."
 startJetty()

b34b1a8f129ce783360daf2aab104159ebd8dc5e
Umberto Nicoletti <umberto.nicoletti@gmail.com>
2015-04-05 08:54:56 +0000

    completed api versioning, minor formatting

--- src/osewh/SendEventWebHook.groovy
+++ src/osewh/SendEventWebHook.groovy
@@ -1,9 +1,9 @@
 package osewh
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.servlet.*
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 def startJetty() {
     Logger logger = LoggerFactory.getLogger(this.class);
@@ -12,7 +12,7 @@ def startJetty() {
 
     def context = new ServletContextHandler(jetty, '/', ServletContextHandler.NO_SESSIONS)
     context.resourceBase = './web/'
-    context.addServlet(new ServletHolder(new OSEWebHookServlet("/api/v1/")), "/*");
+    context.addServlet(new ServletHolder(new OSEWebHookServlet("/api/v1/")), "/*")
 
     logger.info("starting Jetty on 0.0.0.0:${port}, press Ctrl+C to stop")
     jetty.start()
